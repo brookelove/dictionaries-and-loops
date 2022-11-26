18ffc3e761c31cd3a063bb6d051450f8c8fa7369
brookelove <brookelove@Brookes-iMac-2.local>
2022-11-26 12:47:44 -0800

    creating simple instructions

---
+++ amount-in-a-sentence.py
@@ -0,0 +1,6 @@
+'''
+1. create a constant list of integers
+2. using a range function from 55 to 5 with a stop of 10 append each item to a list
+3. take each elment and sum it next to its neighbor, so that 55 + 45 = 100, then 55+ 45+ 35 = 135, then so on and so forth
+    a. two for loops?
+'''
