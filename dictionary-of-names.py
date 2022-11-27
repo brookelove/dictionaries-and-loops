d3c18996fef48e772843d31cb20cd8e4e2100b88
brookelove <brookelove@Brookes-iMac-2.local>
2022-11-26 21:35:26 -0800

    pseudocode for dictionary of names

---
+++ dictionary-of-names.py
@@ -0,0 +1,7 @@
+'''
+1. 2 constants made of last and first names
+2. have to ensure that the last names are greater than the first name 
+    a. if the first name list is longer return an error and exit system
+3. run a for loop that will create a last name as the key and the first name as the value 
+4. if the first name is shorter than the last name return the value as None
+'''
